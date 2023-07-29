29c94bd8e7e420aceab240acd9fb4d9c4bbcbe3a
hinny <mhtsangab@connect.ust.hk>
2023-07-29 23:42:04 +0800

    Add gitsigns.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,5 +20,22 @@ return {
         }
       }
     end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    version = "*",
+    lazy = false,
+    config = function()
+      require("gitsigns").setup {
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol',
+          delay = 0,
+          ignore_whitespace = false,
+        },
+        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+      }
+    end,
   }
 }
