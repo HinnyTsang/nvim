a9a9a71c511f93a48ce4e363eec9ce8c8b0b9096
hinny <mhtsangab@connect.ust.hk>
2023-07-29 23:41:44 +0800

    Update keybindings for switching splits

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -6,7 +6,21 @@ vim.api.nvim_set_keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- system copy
+vim.api.nvim_set_keymap('v', '<C-c>', '"+y', opts)
+
+-- moving line up and down
+vim.api.nvim_set_keymap('n', '<C-k>', ':m-2<CR>', opts)
+vim.api.nvim_set_keymap('n', '<C-j>', ':m+1<CR>', opts)
+
+-- buffer navigation
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', opts)
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', opts)
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', opts)
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', opts)
+
 -- nvim-tree
 vim.api.nvim_set_keymap('n', '<leader>f', ':NvimTreeToggle<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>F', ':NvimTreeFindFile<CR>', opts)
+
 
