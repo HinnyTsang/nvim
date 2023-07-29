2bd03c606ae4a1ea411be6e9d11efb5526e88115
hinny <mhtsangab@connect.ust.hk>
2023-07-29 21:42:56 +0800

    Add nvim-tree

---
+++ lua/plugins/init.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {
+        sort_by = "case_sensitive",
+        view = {
+          adaptive_size = true
+        },
+        filters = {
+          git_ignored = false,
+          dotfiles = false,
+          git_clean = false,
+          no_buffer = false
+        }
+      }
+    end,
+  }
+}
