55d1f3e4e56833172ab3cf9faa6170215358250f
hinny <mhtsangab@connect.ust.hk>
2023-08-02 00:18:55 +0800

    Add plugin lualine

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,6 +27,14 @@ return {
     lazy = false,
     config = function()
       require("gitsigns").setup {
+        signs = {
+          add = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+          change = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+          delete = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+          topdelete = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+          changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+          untracked = { hl = 'GitSignsAdd', text = '┆', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+        },
         current_line_blame = true,
         current_line_blame_opts = {
           virt_text = true,
@@ -37,5 +45,14 @@ return {
         current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
       }
     end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    version = "*",
+    lazy = false,
+    config = function()
+      require("lualine").setup {
+      }
+    end
   }
 }
