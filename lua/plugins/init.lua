462d1b68b1f41dd236cb0c7e1301f8d408803d94
hinny <mhtsangab@connect.ust.hk>
2023-08-02 00:32:33 +0800

    Split nvim-tree to new file

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,26 +1,5 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("nvim-tree").setup {
-        sort_by = "case_sensitive",
-        view = {
-          adaptive_size = true
-        },
-        filters = {
-          git_ignored = false,
-          dotfiles = false,
-          git_clean = false,
-          no_buffer = false
-        }
-      }
-    end,
-  },
+  'nvim-lualine/lualine.nvim',
   {
     "lewis6991/gitsigns.nvim",
     version = "*",
